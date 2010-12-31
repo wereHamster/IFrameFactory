51b91197d5fb5b293d16e90848a0887aa25b9673
Tomas Carnecky <tom@dbservice.com>
2010-12-31 04:46:47 +0100

    Expand tabs

--- Lib/IFrameFactory.lua
+++ Lib/IFrameFactory.lua
@@ -1,6 +1,6 @@
 
 --[[
-	Embedded Library Stub
+  Embedded Library Stub
 ]]
 
 local libName, libMajor, libMinor, libMetatable = "IFrameFactory", "1.0", 0, {
@@ -8,54 +8,54 @@ local libName, libMajor, libMinor, libMetatable = "IFrameFactory", "1.0", 0, {
 }
 
 if (getglobal(libName) == nil) then
-	setglobal(libName, setmetatable({ }, libMetatable))
+  setglobal(libName, setmetatable({ }, libMetatable))
 end
 
 local lib = getglobal(libName)(libMajor)
 if (lib.libVersion and lib.libVersion >= libMinor) then
-	return
+  return
 end
 
 lib.libVersion = libMinor
 
 --[[
-	The AddOn
+  The AddOn
 ]]
 
 lib.Registry = lib.Registry or { }
 
 function lib:Register(group, name, iface)
-	self.Registry[group] = self.Registry[group] or { }
-	self.Registry[group][name] = { Interface = iface, Objects = { { }, { } } }
+  self.Registry[group] = self.Registry[group] or { }
+  self.Registry[group][name] = { Interface = iface, Objects = { { }, { } } }
 end
 
 function lib:Create(group, name)
-	local Info = self.Registry[group][name]
-	local frame = table.remove(Info.Objects[1]) or Info.Interface:Create()
+  local Info = self.Registry[group][name]
+  local frame = table.remove(Info.Objects[1]) or Info.Interface:Create()
 
-	Info.Objects[2][frame] = frame
+  Info.Objects[2][frame] = frame
 
-	frame:ClearAllPoints()
-	frame:Show()
+  frame:ClearAllPoints()
+  frame:Show()
 
-	return frame
+  return frame
 end
 
 function lib:Destroy(group, name, frame)
-	local Info = self.Registry[group][name]
-	frame = Info.Interface:Destroy(frame)
+  local Info = self.Registry[group][name]
+  frame = Info.Interface:Destroy(frame)
 
-	Info.Objects[2][frame] = nil
-	table.insert(Info.Objects[1], frame)
+  Info.Objects[2][frame] = nil
+  table.insert(Info.Objects[1], frame)
 
-	frame:Hide()
+  frame:Hide()
 end
 
 function lib:Clear(group, name)
-	local Info = self.Registry[group][name]
+  local Info = self.Registry[group][name]
 
-	for _, frame in pairs(Info.Objects[2]) do
-		lib:Destroy(group, name, frame)
-	end
+  for _, frame in pairs(Info.Objects[2]) do
+    lib:Destroy(group, name, frame)
+  end
 end
 
